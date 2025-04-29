51a4a0cad9aad6e8fa0399eb24dfe2c7d9b1fb9d
Harsha <harshamidadhala.k@syncfusion.com>
2025-04-29 15:18:26 +0530

    Version updated

--- LogarithmicAxisCustomization_MAUI_CartesianChart/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LogarithmicAxisCustomization_MAUI_CartesianChart;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
---
+++ LogarithmicAxisCustomization_MAUI_CartesianChart/LogarithmicAxisCustomization_MAUI_CartesianChart/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LogarithmicAxisCustomization_MAUI_CartesianChart
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
